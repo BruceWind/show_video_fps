fe40786a1b283d1021f4d65eb05869848d6397aa
weizongwei5 <weizongwei5@gmail.com>
2016-10-28 17:44:55 +0800

    add all file

---
+++ ijkplayer-example/src/main/java/tv/danmaku/ijk/media/example/fragments/SampleMediaListFragment.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright (C) 2015 Zhang Rui <bbcallen@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tv.danmaku.ijk.media.example.fragments;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import tv.danmaku.ijk.media.example.R;
+import tv.danmaku.ijk.media.example.activities.VideoActivity;
+
+public class SampleMediaListFragment extends Fragment {
+    private ListView mFileListView;
+    private SampleMediaAdapter mAdapter;
+
+    public static SampleMediaListFragment newInstance() {
+        SampleMediaListFragment f = new SampleMediaListFragment();
+        return f;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup viewGroup = (ViewGroup) inflater.inflate(R.layout.fragment_file_list, container, false);
+        mFileListView = (ListView) viewGroup.findViewById(R.id.file_list_view);
+        return viewGroup;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        final Activity activity = getActivity();
+
+        mAdapter = new SampleMediaAdapter(activity);
+        mFileListView.setAdapter(mAdapter);
+        mFileListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, final long id) {
+                SampleMediaItem item = mAdapter.getItem(position);
+                String name = item.mName;
+                String url = item.mUrl;
+                VideoActivity.intentTo(activity, url, name);
+            }
+        });
+
+        mAdapter.addItem("rtmp://live.quanmin.tv/live/150", "测试房间");
+        mAdapter.addItem("rtmp://live.quanmin.tv/live/160", "测试房间");
+        mAdapter.addItem("rtmp://live.quanmin.tv/live/2729488", "测试房间");
+        mAdapter.addItem("rtmp://live.quanmin.tv/live/2639525", "测试房间行者");
+
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("输入一个视频流地址");
+
+        final EditText input = new EditText(getActivity());
+        input.setText("rtmp://live.quanmin.tv/live/8299031");
+
+        input.setInputType(InputType.TYPE_CLASS_TEXT );
+        builder.setView(input);
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String m_Text = input.getText().toString();
+                if(!TextUtils.isEmpty(m_Text)) {
+                    VideoActivity.intentTo(activity, m_Text, "用户输入");
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+
+
+
+    }
+
+    final class SampleMediaItem {
+        String mUrl;
+        String mName;
+
+        public SampleMediaItem(String url, String name) {
+            mUrl = url;
+            mName = name;
+        }
+    }
+
+    final class SampleMediaAdapter extends ArrayAdapter<SampleMediaItem> {
+        public SampleMediaAdapter(Context context) {
+            super(context, android.R.layout.simple_list_item_2);
+        }
+
+        public void addItem(String url, String name) {
+            add(new SampleMediaItem(url, name));
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+                view = inflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+            }
+
+            ViewHolder viewHolder = (ViewHolder) view.getTag();
+            if (viewHolder == null) {
+                viewHolder = new ViewHolder();
+                viewHolder.mNameTextView = (TextView) view.findViewById(android.R.id.text1);
+                viewHolder.mUrlTextView = (TextView) view.findViewById(android.R.id.text2);
+            }
+
+            SampleMediaItem item = getItem(position);
+            viewHolder.mNameTextView.setText(item.mName);
+            viewHolder.mUrlTextView.setText(item.mUrl);
+
+            return view;
+        }
+
+        final class ViewHolder {
+            public TextView mNameTextView;
+            public TextView mUrlTextView;
+        }
+    }
+}
